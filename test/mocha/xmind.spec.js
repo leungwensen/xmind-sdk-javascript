a9c6736e3682ad697b9cb27f2e0198512f718dc4
绝云 <wensen.lws@alibaba-inc.com>
2015-07-28 14:57:57 +0800

    add test cases

--- test/mocha/xmind.spec.js
+++ test/mocha/xmind.spec.js
@@ -6,7 +6,8 @@ var assert = require('assert');
 var path = require('path'),
     resolve = path.resolve;
 
-var xmind = require('../../index');
+var xmind = require('../../index'),
+    Workbook = xmind.Workbook;
 
 describe('xmind', function () {
     it('xmind.open', function () {
@@ -17,6 +18,12 @@ describe('xmind', function () {
         }, 'failed to open xmind file');
     });
     it('xmind.save', function () {
+        assert.doesNotThrow(function() {
+            xmind.save(
+                new Workbook('test sheet', 'test topic'),
+                resolve(__dirname, './assets/save-test.xmind')
+            );
+        }, 'failed to save xmind file');
     });
 });
 
