d757ba54da0f71e0c8facc89da8723a324d04282
绝云 <wensen.lws@alibaba-inc.com>
2015-08-04 23:16:13 +0800

    add test cases: - [x] workbook.spec.js - [ ] sheet.spec.js

---
+++ test/mocha/sheet.spec.js
@@ -0,0 +1,11 @@
+/* jshint undef: true, unused: true, node: true */
+/* global require, describe, it */
+
+var assert = require('assert');
+
+var xmind = require('../../index'),
+    Workbook = xmind.Workbook;
+
+describe('Sheet', function () {
+});
+
--- test/mocha/workbook.spec.js
+++ test/mocha/workbook.spec.js
@@ -10,15 +10,151 @@ var xmind = require('../../index'),
     Workbook = xmind.Workbook;
 
 describe('Workbook', function () {
-    it('creating with first sheet name and root topic name', function () {
+    var options = {
+        firstSheetId: 'firstSheet',
+        rootTopicId: 'rootTopic',
+        firstSheetName: 'sheet 1',
+        rootTopicName: 'root topic',
+    };
+    var workbook,
+        sheet,
+        rootTopic;
+
+    it('new Workbook(options)', function () {
         assert.doesNotThrow(function() {
-            console.log(new Workbook({
-                firstSheetName: 'hello',
-                rootTopicName: 'world',
-            }).toJSON());
+            workbook = new Workbook(options); // first sheet added
+            sheet = workbook.getPrimarySheet();
+            rootTopic = sheet.rootTopic;
         }, 'failed to create a Workbook instance');
     });
-    it('creating with doc, stylesDoc and attachments', function () {
+    it('workbook.sheetById', function () {
+        assert.equal(
+            workbook.sheetById[options.firstSheetId],
+            sheet,
+            'workbook.sheetById is not working'
+        );
+    });
+    it('workbook.sheets', function () {
+        assert.equal(
+            workbook.sheets[0],
+            sheet,
+            'workbook.sheets is not working'
+        );
+    });
+    it('workbook.getPrimarySheet()', function () {
+        assert.equal(
+            options.firstSheetName,
+            sheet.getTitle(),
+            'sheet name unmatched'
+        );
+        assert.equal(
+            options.rootTopicName,
+            rootTopic.getTitle(),
+            'root topic name unmatched'
+        );
+    });
+
+    var secondSheetOptions = {
+        id: 'secondSheet',
+        title: 'sheet 2',
+        rootTopicId: 'rootTopic',
+        rootTopicName: 'root topic',
+    };
+    it('workbook.addSheet()', function () {
+        assert.throws(function() {
+            workbook.addSheet({
+                id: options.firstSheetId, // duplicated id
+                title: '',
+                rootTopicId: 'some id',
+                rootTopicName: 'some name',
+            });
+        }, 'can create a sheet with a duplicated id');
+        assert.doesNotThrow(function() {
+            workbook.addSheet(secondSheetOptions); // second sheet added
+        }, 'failed to execute workbook.addSheet(options)');
+    });
+    it('workbook.moveSheet(fromIndex, toIndex)', function () {
+        assert.doesNotThrow(function() {
+            workbook.moveSheet(1, 0);
+        }, 'failed to execute workbook.moveSheet(fromIndex, toIndex)');
+        assert.equal(
+            workbook.doc.firstChild.getAttribute('id'),
+            workbook.sheets[0].doc.getAttribute('id'),
+            'workbook.moveSheet(fromIndex, toIndex) not working: xml structure is unchanged'
+        );
+        assert.notEqual(
+            workbook.getPrimarySheet(),
+            sheet,
+            'workbook.moveSheet(fromIndex, toIndex) not working'
+        );
+    });
+    it('workbook.removeSheet(/* id or index or Sheet instance */)', function () {
+        // remove by id {
+            assert.doesNotThrow(function() {
+                workbook.removeSheet(secondSheetOptions.id); // secondSheet
+            }, 'failed to execute workbook.removeSheet(id)');
+            assert.equal(
+                workbook.doc.childNodes.length,
+                1,
+                'workbook.removeSheet(id) not working: xml structure is unchanged'
+            );
+            assert.equal(
+                workbook.sheets.length,
+                1,
+                'workbook.removeSheet(id) not working: workbook.sheets is unchanged'
+            );
+            assert.ok(
+                !workbook.sheetById[secondSheetOptions.id],
+                'workbook.removeSheet(id) not working: workbook.sheetById is unchanged'
+            );
+        // }
+        // remove by index {
+            workbook.addSheet(secondSheetOptions); // reverse
+            assert.doesNotThrow(function() {
+                workbook.removeSheet(1); // secondSheet
+            }, 'failed to execute workbook.removeSheet(index)');
+            assert.equal(
+                workbook.doc.childNodes.length,
+                1,
+                'workbook.removeSheet(index) not working: xml structure is unchanged'
+            );
+            assert.equal(
+                workbook.sheets.length,
+                1,
+                'workbook.removeSheet(index) not working: workbook.sheets is unchanged'
+            );
+            assert.ok(
+                !workbook.sheetById[secondSheetOptions.id],
+                'workbook.removeSheet(index) not working: workbook.sheetById is unchanged'
+            );
+        // }
+        // remove by instance {
+            var secondSheet = workbook.addSheet(secondSheetOptions); // reverse
+            assert.doesNotThrow(function() {
+                workbook.removeSheet(secondSheet); // secondSheet
+            }, 'failed to execute workbook.removeSheet(index)');
+            assert.equal(
+                workbook.doc.childNodes.length,
+                1,
+                'workbook.removeSheet(index) not working: xml structure is unchanged'
+            );
+            assert.equal(
+                workbook.sheets.length,
+                1,
+                'workbook.removeSheet(index) not working: workbook.sheets is unchanged'
+            );
+            assert.ok(
+                !workbook.sheetById[secondSheetOptions.id],
+                'workbook.removeSheet(index) not working: workbook.sheetById is unchanged'
+            );
+        // }
+    });
+    it('workbook.destroy()', function () {
+        workbook.destroy();
+        assert.ok(
+            workbook,
+            'workbook should not be destroyed'
+        );
     });
 });
 
